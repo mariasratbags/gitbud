1d24f165d5f7a681be86a67143930968272d714c
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-10 14:54:41 -0700

    Add Airbnb stateless component example

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-// var Main = () => (<h1>Hello, world!</h1>);
-//
-// export default Main;
-
-class Main extends React.Component {
-  render() {
-    return (
-      <div>Hello world!</div>
-    )
-  }
+//Airbnb stateless component style guide
+function Main() {
+  return <div>Hello world!</div>
 }
 
-module.exports = Main;
+export default Main;
