22b0c3b1ceb17f60608fe137144bfd7a17c66e5c
P-WM <P-WM@users.noreply.github.com>
2017-08-10 23:59:43 +0100

    serving dummy data on /users endpoint

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 /*
-Organize files as separate modules
-Have a file for routes
-Have a file for helper functions
-For stage 0, return hello world to client on '/' GET request
-Serve dummy user data to user endpoints
-Serve static files from dist
+[x] Organize files as separate modules
+[x] Have a file for routes
+[ ] Have a file for helper functions
+[x] For stage 0, return hello world to client on '/' GET request
+[x] Serve dummy user data to user endpoints
+[x] Serve static files from dist
 */
 const express = require('express');
 const path = require('path');
+const routes = require('./server/routes');
 
 // make express server
 const app = express();
@@ -18,6 +19,8 @@ app.listen(port, () => {
 
 // serve static files and hello world
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.use((req, res) => {
+  console.log(req.url);
+  routes[req.method][req.url](req)
+    .then(res.send);
 });
