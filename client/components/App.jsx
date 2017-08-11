0c5ddd1ab3c2608c481e8f2db0306db8c4ea4e76
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-10 21:49:40 -0700

    Fetch data via axios and render onto User page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './Landing';
 import User from './User';
 
 class App extends React.Component {
+
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path='/user' component={User} />
+          <Route exact path='/user' component={User}/>
           <Route exact path='/' component={Landing} />
         </div>
       </BrowserRouter>
