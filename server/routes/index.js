faf3c75c4c827b8d6d24407965740bd361fa5a89
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-10 21:13:03 -0700

    Hack to get user page to render

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.routes = {
   GET: {
     '/': function getRoot() {
@@ -13,6 +16,22 @@ exports.routes = {
           { name: 'Brian' },
           { name: 'Shaikat' },
         ]);
+      });
+    },
+    '/user': function getUser() {
+      return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, '../../dist/index.html'), 'utf8', (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    },
+    '/favicon.ico': function getFavicon() {
+      return new Promise((resolve) => {
+        resolve();
       });
     },
   },
