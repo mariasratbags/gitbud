9a351cb82f18cd3253f0a097dc88d34a5be28ff5
P-WM <P-WM@users.noreply.github.com>
2017-08-15 16:37:39 +0100

    Create material-ui UserList in ProjectDetails.

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
 class UserList extends React.Component {
   constructor(props) {
@@ -10,10 +18,24 @@ class UserList extends React.Component {
   render() {
     return (
       <div>
+        <Table>
+          <TableHeader displaySelectAll={ false } >
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Rating</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={ false }>
+            {this.props.users.map(user => <TableRow>
+                <TableRowColumn><Link to={`/user/${user.name}`}>{ user.name }</Link></TableRowColumn>
+                <TableRowColumn>{ user.rating }</TableRowColumn>
+              </TableRow>)}
+          </TableBody>
+        </Table>
         <ul>
-          {this.props.users.map(user => <li><Link to={`user/${user.name}`}>{user.name}</Link></li>)}
+          {this.props.users.map(user => <li><Link to={`user/${user.name}`}>{JSON.stringify(user)}</Link></li>)}
         </ul>
-      </div>
+      </div >
     );
   }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
