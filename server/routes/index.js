3eca7547cc92dcf022a8ae6802958e5a39973290
P-WM <P-WM@users.noreply.github.com>
2017-08-16 23:33:40 +0100

    Move some /auth routes to routes module.

--- server/routes/index.js
+++ server/routes/index.js
@@ -54,3 +54,27 @@ exports.api = {
     },
   },
 };
+
+exports.auth = {
+  GET: {
+    signout: function signout(req, res) {
+      // destroy session and redirect to home
+      req.logout();
+      res.redirect('/');
+    },
+    authenticated: function checkAuthenticated(req, res) {
+      // Confirm if user signed in
+      res.send(req.isAuthenticated());
+    },
+    // Currently server handling--possibly review
+    // github: function callback(req, res, urlParts) {
+    //   // upon successful authentication, redirect to projects
+    //   if (urlParts[3] === 'callback') {
+    //     res.redirect('/projects');
+    //   } else {
+    //     res.statusCode = 400;
+    //     res.send('Invalid request');
+    //   }
+    // }
+  }
+}
