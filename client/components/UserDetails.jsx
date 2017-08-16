e0897ff9a3b136faaefc5d3338b829e0bd2cc24d
P-WM <P-WM@users.noreply.github.com>
2017-08-17 00:42:23 +0100

    Remove comments and fix indentation.

--- client/components/UserDetails.jsx
+++ client/components/UserDetails.jsx
@@ -15,7 +15,6 @@ class UserDetails extends React.Component {
     this.state = {
       expanded: false
     }
-    // this.props.user = this.props.users[this.props.users.indexOf(this.props.id)];
     this.expandCard = () => this.setState({ expanded: true });
   }
 
@@ -25,7 +24,7 @@ class UserDetails extends React.Component {
       <Card style={ {width: '40%', margin: 'auto', marginTop: 12, padding: 12 } }>
         <Card expanded={ this.state.expanded }>
           <CardMedia overlay={ <CardTitle title={ this.props.user.name } subtitle='Experience: n00b'/> }>
-             <img src={ this.props.user.avatarUrl } />
+            <img src={ this.props.user.avatarUrl } />
           </CardMedia>
           <div style={ { width: '35%', float: 'right', padding: 15 } }>
             <RaisedButton label='Message Me' style={ { width: '60%' } } fullWidth={ true } icon={ <ActionFace /> } onClick={ this.expandCard } primary={ true }/>
