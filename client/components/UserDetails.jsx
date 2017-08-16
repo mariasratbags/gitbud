6c962b38990189ba71873d9c64a937171a1941d4
P-WM <P-WM@users.noreply.github.com>
2017-08-17 00:41:22 +0100

    Add avatar to UserDetails component.

--- client/components/UserDetails.jsx
+++ client/components/UserDetails.jsx
@@ -15,19 +15,8 @@ class UserDetails extends React.Component {
     this.state = {
       expanded: false
     }
-
+    // this.props.user = this.props.users[this.props.users.indexOf(this.props.id)];
     this.expandCard = () => this.setState({ expanded: true });
-  }
-
-  getIndexFromId(id) {
-    var numberId = parseInt(id, 10);
-    var index;
-    for (var i = 0; i < this.props.users.length; i++) {
-      if( numberId === this.props.users[i].id) {
-        index = i;
-      }
-    }
-    return index;
   }
 
   render() {
@@ -35,8 +24,8 @@ class UserDetails extends React.Component {
     return (
       <Card style={ {width: '40%', margin: 'auto', marginTop: 12, padding: 12 } }>
         <Card expanded={ this.state.expanded }>
-          <CardMedia overlay={ <CardTitle title={this.props.users[this.getIndexFromId(this.props.match.params.id)].name} subtitle='Experience: n00b'/> }>
-            <img src='https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAyLAAAAJDQ4YzMwZTNlLTAwODItNGYwMC1iMGQxLWYzOGZiZjM5YWE3NQ.jpg'/>
+          <CardMedia overlay={ <CardTitle title={ this.props.user.name } subtitle='Experience: n00b'/> }>
+             <img src={ this.props.user.avatarUrl } />
           </CardMedia>
           <div style={ { width: '35%', float: 'right', padding: 15 } }>
             <RaisedButton label='Message Me' style={ { width: '60%' } } fullWidth={ true } icon={ <ActionFace /> } onClick={ this.expandCard } primary={ true }/>
@@ -59,9 +48,10 @@ class UserDetails extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, props) => {
+  const userId = Number(props.match.params.id);
   return {
-    users: state.users,
+    user: state.users.filter(user => user.id === userId)[0]
   };
 };
 
