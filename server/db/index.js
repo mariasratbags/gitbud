db8d685d5d663a62cfce0d41d0a2b33b241bc8bc
P-WM <P-WM@users.noreply.github.com>
2017-08-16 15:17:12 +0100

    Tidy up db exports.

--- server/db/index.js
+++ server/db/index.js
@@ -5,11 +5,4 @@ const url = process.env.GRAPHENEDB_BOLT_URL || 'bolt://localhost';
 const username = process.env.GRAPHENEDB_BOLT_USERNAME || 'neo4j';
 const password = process.env.GRAPHENEDB_BOLT_PASSWORD || 'neo';
 // connect and create session
-const driver = neo4j.driver(url, neo4j.auth.basic(username, password));
-const session = driver.session();
-// make available to other modules
-exports.runQuery = session.run.bind(session);
-
-// exports.runQuery('MATCH (p {name: "Shaikat"}) RETURN p')
-//   .then(console.log)
-//   .catch(console.error);
+exports.driver = neo4j.driver(url, neo4j.auth.basic(username, password));
