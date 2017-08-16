e9395462d4bab25f264f16bdc83d93fbd5cb21ed
P-WM <P-WM@users.noreply.github.com>
2017-08-16 02:40:50 +0100

    Change UserDetails name to overlay.

--- client/components/UserDetails.jsx
+++ client/components/UserDetails.jsx
@@ -9,19 +9,15 @@ function UserDetails() {
   return (
     <Card style={ {width: '40%', margin: 'auto', marginTop: 12, padding: 12 } }>
       <Card>
-        <CardMedia>
+        <CardMedia overlay={ <CardTitle title='Username' subtitle='Experience: n00b'/> }>
           <img src='https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAyLAAAAJDQ4YzMwZTNlLTAwODItNGYwMC1iMGQxLWYzOGZiZjM5YWE3NQ.jpg'/>
         </CardMedia>
-        <Toolbar>
-          <ToolbarTitle text="Username"/>
-        </Toolbar>
-        <div style={ { width: '35%', float: 'right', padding: 12 } }>
+        <div style={ { width: '35%', float: 'right', padding: 15 } }>
           <RaisedButton label='Message Me' style={ { width: '60%' } } fullWidth={ true } icon={ <ActionFace/> } primary={ true }/>
         </div>
         <div style={ { width: '60%' } }>
           <CardTitle title='Languages' subtitle='JavaScript, React, Spanish'/>
         </div>
-        <CardTitle title='Level' subtitle='n00b'/>
         <CardTitle title='Projects' subtitle='N Queens, Hello World'/>
       </Card>
     </Card>
