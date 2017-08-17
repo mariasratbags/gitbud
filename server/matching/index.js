b67d5399ef56c834805e5c8a9682ace22ce06d94
P-WM <P-WM@users.noreply.github.com>
2017-08-17 21:49:48 +0100

    Check for non-commites or forks. Long (and rather hairy) Promise chain to work with all this information. It also doubles up on forks, so need to get my head round GitHub API better.

--- server/matching/index.js
+++ server/matching/index.js
@@ -1,29 +1,80 @@
-const db = require('../db');
+// Libraries
 const axios = require('axios');
-const _map = require('lodash/map');
+
+// lodash functions
+const map = require('lodash/map');
+const forEach = require('lodash/forEach');
+const filter = require('lodash/filter');
+
+// Other server modules
+const db = require('../db');
 const getLanguages = require('./languages');
 
+// Gets all repos that a user either created or has committed to
 exports.getUserRepos = function getUserRepos(ghId) {
-  var OAuthToken;
+  // First get user info from the db
   const dbSession = db.driver.session();
   return dbSession.run(`
     MATCH (user:User {ghId: ${ ghId }}) RETURN user
   `)
     .then((res) => {
-      OAuthToken = new db.models.User(res.records[0].get('user')).OAuthToken;
+      // Then use that info to get the users repos from GitHub
+      const { OAuthToken, username } = new db.models.User(res.records[0].get('user'));
       return axios.get('https://api.github.com/user/repos', {
         params: {
           access_token: OAuthToken,
+          per_page: 100,
         }
       })
+        .then(res => ({ OAuthToken, username, res }));
     })
-    .then(res =>
-      ({ OAuthToken, repos: _map(res.data, ({ name, owner, languages_url }) => 
-        ({ name, owner: owner.login, languages_url }))
+    .then(({OAuthToken, username, res}) =>
+      // Then extract the useful information from those repos
+      // Here we're interested in the owner, name, whether it's forked and the url for its language stats
+      ({
+        OAuthToken,
+        username,
+        repos: map(res.data, ({ name, owner, languages_url, fork }) => 
+          ({ name, owner: owner.login, languages_url, fork }))
+      })
+    )
+    .then(({ username, OAuthToken, repos }) => {
+      // Then check if they're forks. If so, find out whether or not the user has actually committed to that repo.
+      const commitPromises = [];
+      forEach(repos, (repo) => {
+        if (repo.fork) {
+          // It's a fork. so qury for commits
+          commitPromises.push(axios.get(`https://api.github.com/repos/${ username }/${ repo.name }/commits`, {
+            params: {
+              access_token: OAuthToken,
+              author: username,
+            },
+          })
+            .then((res) => {
+              // Check for commits
+              repo.include = res.data.length > 0;
+            })
+          );
+        } else {
+          // Not a fork.
+          repo.include = true;
+        }
+      });
+      return Promise.all(commitPromises)
+        .then(() => ({ username, OAuthToken, repos }));
+    })
+    .then(({ OAuthToken, username, repos }) => 
+      // Finally filter by the include property
+      ({
+        OAuthToken,
+        username,
+        repos: filter(repos, repo => repo.include),
       })
     );
 };
 
+// This is a testing function which uses the above chain
+// and one from the languages module to show a user's most-used languages
 exports.displayUserLanguages = function gl(ghId) {
   exports.getUserRepos(ghId)
     .then(getLanguages.getUserLanguages)
