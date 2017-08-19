a762881f4cf46685e7384e561b5c960f838e6541
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-18 20:25:32 -0700

    Decrease the width of container

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -7,13 +7,17 @@ import RaisedButton from 'material-ui/RaisedButton';
 // makes the theme available via a higher-0rder component
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
+const style = {
+  textAlign: 'center',
+};
+
 function Landing(props) {
   if (props.checkAuth) {
     props.checkAuth();
   }
   return (
-    <Card style={ { width: '50%', margin: 'auto', paddingBottom: 12, marginTop: 12 } }>
-      <AppBar title="Welcome to GitBud!" showMenuIconButton={ false }/>
+    <Card style={ { width: '25%', margin: 'auto', paddingBottom: 12, marginTop: 12 } }>
+      <AppBar title="Welcome to GitBud!" style={style} showMenuIconButton={ false }/>
       <CardMedia>
         <img src="https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"/>
       </CardMedia>
