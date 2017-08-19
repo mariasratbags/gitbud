9037fb67a33453265fb46c4b02a6e00e239623d2
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-18 17:26:46 -0700

    Edit contributing.md

--- client/components/ProjectStatus.jsx
+++ client/components/ProjectStatus.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function ProjectStatus(props) {
   return (
     <div>
-      <pre><code>{JSON.stringify(props.match.params)}</code></pre>
       <p>This is ProjectStatus component</p>
     </div>
   );
