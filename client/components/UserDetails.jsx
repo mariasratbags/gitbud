a48548cef07c2dcd10457383c0118cf2134463b6
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-18 21:11:40 -0700

    Edit UserDetails page

--- client/components/UserDetails.jsx
+++ client/components/UserDetails.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import Paper from 'material-ui/Paper';
+import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 import { Card, CardMedia, CardText, CardTitle } from 'material-ui/Card';
-import { Toolbar, ToolbarTitle } from 'material-ui/Toolbar';
 import { fullWhite } from 'material-ui/styles/colors';
 import RaisedButton from 'material-ui/RaisedButton';
 import ActionFace from 'material-ui/svg-icons/action/face';
@@ -24,9 +25,9 @@ class UserDetails extends React.Component {
     this.togglePair = this.togglePair.bind(this);
     this.pairButton = () => {
       if (this.props.user.paired) {
-        return <RaisedButton label='Partnered' style={ { margin: 2 } } labelColor={ fullWhite } backgroundColor='#a4c639' fullWidth={ true } icon={ <ActionDone color={ fullWhite } /> } onClick={ this.togglePair } />
+        return <RaisedButton label='Partnered' labelColor={ fullWhite } backgroundColor='#a4c639' fullWidth={true} icon={ <ActionDone color={ fullWhite } /> } onClick={ this.togglePair } />
       } else if (this.props.match.params.projectId) {
-        return <RaisedButton label='Work With Me' style={ { margin: 2 } } fullWidth={ true } icon={ <ActionBuild /> } onClick={ this.togglePair } primary={ true } />
+        return <RaisedButton label='Work With Me' fullWidth={true} icon={ <ActionBuild /> } onClick={ this.togglePair } primary={ true } />
       }
     };
   }
@@ -47,29 +48,34 @@ class UserDetails extends React.Component {
 
   render() {
     return (
-      <Card style={ {width: '40%', margin: 'auto', marginTop: 12, padding: 12 } }>
-        <Card expanded={ this.state.expanded }>
-          <CardMedia overlay={ <CardTitle title={ this.props.user.name } subtitle='Experience: n00b'/> }>
-            <img src={ this.props.user.avatarUrl } />
-          </CardMedia>
-          <div style={ { width: '35%', float: 'right', padding: 15 } }>
+      <Paper style={{ width: '95%', margin: 'auto', marginTop: 12, padding: 12 }}>
+        <Card expanded={ this.state.expanded } style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto', marginBottom: 12 }}>
+          <Toolbar>
+            <ToolbarGroup>
+              <ToolbarTitle text="User Profile" />
+            </ToolbarGroup>
+          </Toolbar>
+          <CardTitle title={ this.props.user.name } subtitle="Experience: Beginner" />
+          <div>
+            <CardTitle title="Languages" subtitle='JavaScript, React, Spanish'/>
+          </div>
+          <CardTitle title="Projects" subtitle='N Queens, Hello World'/>
+          <div>
             { this.pairButton() }
-            <RaisedButton label='Message Me' style={ { margin: 2 } } fullWidth={ true } icon={ <ActionFace /> } onClick={ this.expandCard } secondary={ true }/>
+            <RaisedButton label='Message Me' fullWidth={true} icon={<ActionFace />} onClick={this.expandCard} secondary={true} />
           </div>
-          <div style={ { width: '60%' } }>
-            <CardTitle title='Languages' subtitle='JavaScript, React, Spanish'/>
+          <div expandable={true}>
+            <TextField
+              floatingLabelText="Ask user to pair up"
+              hintText="Enter your message"
+              style={{ padding: 20 }}
+            />
           </div>
-          <CardTitle title='Projects' subtitle='N Queens, Hello World'/>
-          <div style={ { width: '35%', float: 'right', padding: 15 } } expandable={ true }>
-            <RaisedButton label='Send' style={ { width: '60%' } } fullWidth={ true } icon={ <ContentSend /> } secondary={ true }/>
-          </div>
-          <div style={ { width: '60%' } } expandable={ true }>
-            <TextField multiLine={ true }
-              floatingLabelText="Invite Username to code with you" hintText="Enter your message"
-              rows={ 2 } style={ { padding: 20 } }/>
+          <div expandable={true}>
+            <RaisedButton label="Send" fullWidth={true} icon={<ContentSend />} secondary={true}/>
           </div>
         </Card>
-      </Card>
+      </Paper>
     );
   }
 }
@@ -87,3 +93,9 @@ const mapDispatchToProps = dispatch =>
   });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserDetails);
+
+
+
+{/* <CardMedia overlay={ <CardTitle title={ this.props.user.name } subtitle='Experience: n00b'/> }>
+  <img src={ this.props.user.avatarUrl } />
+</CardMedia> */}
