bfc89419e5d0e0b0082f9fe8b216422da9c16469
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-20 08:41:32 -0700

    Edit styles on MyProjects page

--- client/components/MyProjects.jsx
+++ client/components/MyProjects.jsx
@@ -11,29 +11,42 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import {
+  Toolbar,
+  ToolbarGroup,
+  ToolbarTitle
+} from 'material-ui/Toolbar';
+import {Card, CardText } from 'material-ui/Card';
 
 const MyProjects = (props) => {
   return (
-    <Paper>
-      <Table>
-        <TableHeader displaySelectAll={ false }>
-          <TableRow>
-            <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Language</TableHeaderColumn>
-            <TableHeaderColumn>Experience</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody stripedRows={ true }>
-          {props.projects.map(project =>
-            (<TableRow key={ project.id }>
-              <TableRowColumn><Link to={`/projects/${ project.id }`}>{ project.project }</Link></TableRowColumn>
-              <TableRowColumn>{ project.language }</TableRowColumn>
-              <TableRowColumn>{ project.experience }</TableRowColumn>
-            </TableRow>)
-          )}
-        </TableBody>
-      </Table>
-      <RaisedButton label="I'm interested" primary={ true } fullWidth={ true } />
+    <Paper style={ {width: '95%', margin: 'auto', marginTop: 12, padding: 12 } }>
+      <Card>
+        <Toolbar>
+          <ToolbarGroup>
+            <ToolbarTitle text="My Projects"/>
+          </ToolbarGroup>
+        </Toolbar>
+        <Table>
+          <TableHeader displaySelectAll={ false }>
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Language</TableHeaderColumn>
+              <TableHeaderColumn>Experience</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody stripedRows={ true } displayRowCheckbox={ false }>
+            {props.projects.map(project =>
+              (<TableRow key={ project.id }>
+                <TableRowColumn><Link to={`/projects/${ project.id }`}>{ project.project }</Link></TableRowColumn>
+                <TableRowColumn>{ project.language }</TableRowColumn>
+                <TableRowColumn>{ project.experience }</TableRowColumn>
+              </TableRow>)
+            )}
+          </TableBody>
+        </Table>
+      {/* <RaisedButton label="I'm interested" primary={ true } fullWidth={ true } /> */}
+    </Card>
     </Paper>
   );
 };
