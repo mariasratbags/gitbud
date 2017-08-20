44e4f452a4abfd5574747eeaaf33e8670ac0b402
Brian Kim <briankim4311@gmail.com>
2017-08-20 12:29:51 -0700

    Implement handleCheck for checkboxes in Questionnaire

--- client/components/Questionnaire.jsx
+++ client/components/Questionnaire.jsx
@@ -7,45 +7,58 @@ import Checkbox from 'material-ui/Checkbox';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-function Questionnaire() {
-  return (
-    <Card style={ { width: '50%', margin: 'auto', padding: 12, marginTop: 12 } }>
-      <h1>Welcome, FIRST_NAME.</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius quam id quam aliquot, quis varius est euismod.</p>
-      <br />
-      <p>Select your preferred langauge to use with other GitBud members:</p>
-      <DropDownMenu value={'JavaScript'}>
-        <MenuItem value={'JavaScript'} primaryText="JavaScript" />
-        <MenuItem value={'Ruby'} primaryText="Ruby" />
-        <MenuItem value={'Python'} primaryText="Python" />
-        <MenuItem value={'PHP'} primaryText="PHP" />
-      </DropDownMenu>
-      <br />
-      <p>Select your proficieny level at the chosen language above:</p>
-      <RadioButtonGroup>
-        <RadioButton value="Beginner" label="Beginner" />
-        <RadioButton value="Intermediate" label="Intermediate" />
-        <RadioButton value="Advanced" label="Advanced" />
-      </RadioButtonGroup>
-      <br />
-      <p>Check all languages you are interested or have experience in:</p>
-      <Checkbox label="JavaScript" />
-      <Checkbox label="Ruby" />
-      <Checkbox label="Python" />
-      <Checkbox label="PHP" />
-      <Checkbox label="Perl" />
-      <Checkbox label="Java" />
-      <Checkbox label="C" />
-      <Checkbox label="C++" />
-      <Checkbox label="C#" />
-      <Checkbox label="Objective-C" />
-      <br />
-      <p>Write a short introduction about yourself that other GitBud members can see:</p>
-      <TextField multiLine={ true } rows={ 2 } style ={ { width: '100%' } } />
-      <br />
-      <RaisedButton label="Submit" secondary={ true }  fullWidth={ true } />
-    </Card>
-  );
+class Questionnaire extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguages: [],
+    };
+  }
+
+  handleCheck(e, b) {
+    e = e.target.value;
+    if (b) {
+      this.setState({ selectedLanguages: this.state.selectedLanguages.concat([e]) }, () => console.log(this.state.selectedLanguages));
+    } else {
+      let index = this.state.selectedLanguages.indexOf(e);
+      let copy = this.state.selectedLanguages.slice(0);
+      copy.splice(index, 1);
+      this.setState({ selectedLanguages: copy }, () => console.log(this.state.selectedLanguages));
+    }
+  }
+
+  render() {
+    return (
+      <Card style={ { width: '50%', margin: 'auto', padding: 12, marginTop: 12 } }>
+        <h1>Welcome, FIRST_NAME.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius quam id quam aliquot, quis varius est euismod.</p>
+        <br />
+        <p>Check all languages you are interested or, b have experience in:</p>
+        <Checkbox label="JavaScript" value="JavaScript" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="Ruby" value="Ruby" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="Python" value="Python" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="PHP" value="PHP" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="Perl" value="Perl" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="Java" value="Java" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="C++" value="C++" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="C#" value="C#" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <Checkbox label="Objective-C" value="Objective-C" onCheck={(e, b) => this.handleCheck(e, b)} />
+        <br />
+
+        <p>Select your proficieny level at the chosen languages above:</p>
+        <RadioButtonGroup>
+          <RadioButton value="Beginner" label="Beginner" />
+          <RadioButton value="Intermediate" label="Intermediate" />
+          <RadioButton value="Advanced" label="Advanced" />
+        </RadioButtonGroup>
+        <br />
+        <p>Write a short introduction about yourself that other GitBud members can see:</p>
+        <TextField multiLine={ true } rows={ 2 } style ={ { width: '100%' } } />
+        <br />
+        <RaisedButton label="Submit" secondary={ true }  fullWidth={ true } />
+      </Card>
+    );
+  }
 }
 
 export default Questionnaire;
