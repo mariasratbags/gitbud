ccbbb79d384add9be843e98b6345ba5675f00e2d
Francis Ngo <francis.t.ngo@gmail.com>
2017-08-21 22:51:23 -0700

    Add introductions to index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,12 @@
-  /*
-    This is the entry point where we inject the App component into an element on index.html
+/*
+  This is the entry point where we inject the App component into an element on index.html
 
-    In order for child components to get access to the Redux store, Provider allows them to be subscribed to it. Provider magically makes the store available to all components in the application without passing it explicitly. This is only used once when you render the root component (App).
-    LINK: http://redux.js.org/docs/basics/UsageWithReact.html
+  In order for child components to get access to the Redux store, Provider allows them to be subscribed to it. Provider magically makes the store available to all components in the application without passing it explicitly. This is only used once when you render the root component (App).
+  LINK: http://redux.js.org/docs/basics/UsageWithReact.html
 
-    In order to use Material-UI React Components, we must use MuiThemeProvider
-    LINK: http://www.material-ui.com/#/get-started/usage
-  */
+  In order to use Material-UI React Components, we must use MuiThemeProvider
+  LINK: http://www.material-ui.com/#/get-started/usage
+ */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
