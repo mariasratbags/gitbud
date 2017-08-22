8836f85d76ec26d8b86154bdd270e7d3b2389805
P-WM <P-WM@users.noreply.github.com>
2017-08-22 15:44:11 +0100

    Add explanatory comments and hints.

--- server/profiling/matching.js
+++ server/profiling/matching.js
@@ -1,3 +1,25 @@
+/*
+ *  MATCHING
+ *  (Used as part of a Promise chain in the authentication module for handling new users.
+ *  Invoked after the new user's  experience profile has been built in /profiling/index.js)
+ * 
+ * This implements an incredibly rudimentary matching algorithm, essentially by establishing a user's
+ * nearest neighbours. It does this by finding the 'distance' between the new user and every other
+ * across several dimensions and using the Pythagorean theorem to squash that distance down into one dimension.
+ * I'm not very mathsy, so if you think that any element of this is silly or wrong... you're probably right!
+ * 
+ * THINGS TO FIX:
+ * 
+ * Currently, this is composed of a number of asynchronous components and only a few users, so the computationally
+ * expensive components are A) not too expensive and B) frequntly interrupted by other requests. Consequently, this won't
+ * make the server unresponsive. However, as the number of users grows, node's single threaded model may cause problems here.
+ * Possible solutions are to:
+ *  --Keep the server responsive by having multiple threads handle requests using cluster.fork();
+ *  --Offload this processing to a separate thread once the user is authenticated using child_process.fork();
+ *  --Run this as a cron job, regularly checking for new users and matching them.
+ * 
+ */
+
 // Libraries
 const forEach = require('lodash/forEach');
 const map = require('lodash/map');
