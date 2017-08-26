554a3567f2cbfbcc6b39cb4f59c56c26d54f0a80
Harvey Multani <harvey.multani@gmail.com>
2017-08-25 21:14:09 -0700

    Remove n variable

--- server/profiling/repoStats.js
+++ server/profiling/repoStats.js
@@ -1,19 +1,18 @@
 /*
  *  REPO STATS
  *  (Invoked from /server/profiling/index.js)
- * 
+ *
  *  This module simply tots up all the repo stats and adds them to the profile.
- * 
+ *
  *  It doesn't do anything clever or special, but it certainly could with more information
  *  from GitHub (tips about using graphQL in profilin/index.js).
- * 
+ *
  */
 
 module.exports = function getRepoStats({ OAuthToken, username, repos, profile }) {
   let repoStats = 0;
-  let i;
-  for (i = 0, n = repos.length; i < n; i++) {
-    repoStats += repos[i].forks_count 
+  for (let i = 0; i < repos.length; i++) {
+    repoStats += repos[i].forks_count
       + repos[i].stargazers_count
       + repos[i].watchers_count;
   }
